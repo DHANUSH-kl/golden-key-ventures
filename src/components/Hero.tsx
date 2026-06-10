28900f7d363ed0a7861f62e335f30bb9b990b6b9
DHANUSH-KL <dhanuchandu1232@gmail.com>
2026-06-10 16:37:38 +0530

    newewqrwedwasadsgdgdsadfjnsdakreafsdrf

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,72 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import './Hero.css';
 
+const exteriorImages = [
+  '/exterior/Enscape_2023-12-02-12-32-02.png',
+  '/exterior/Enscape_2023-12-02-12-33-07.png',
+  '/exterior/Enscape_2023-12-02-12-37-39.png'
+];
+
+const interiorImages = [
+  '/interior/1st-F-Interior/1st F Interior_page-0001.jpg',
+  '/interior/1st-F-Interior/1st F Interior_page-0002.jpg',
+  '/interior/1st-F-Interior/1st F Interior_page-0003.jpg',
+  '/interior/1st-F-Interior/1st F Interior_page-0007.jpg',
+  '/interior/1st-F-Interior/1st F Interior_page-0010.jpg',
+  '/interior/1st-F-Interior/1st F Interior_page-0011.jpg',
+  '/interior/1st-F-Interior/1st F Interior_page-0013.jpg',
+  '/interior/1st-F-Interior/1st F Interior_page-0014.jpg',
+  '/interior/1st-F-Interior/1st F Interior_page-0016.jpg',
+  '/interior/G-F-Interior/G F Interior_page-0001.jpg',
+  '/interior/G-F-Interior/G F Interior_page-0002.jpg',
+  '/interior/G-F-Interior/G F Interior_page-0004.jpg',
+  '/interior/G-F-Interior/G F Interior_page-0005.jpg',
+  '/interior/G-F-Interior/G F Interior_page-0007.jpg',
+  '/interior/G-F-Interior/G F Interior_page-0008.jpg',
+  '/interior/G-F-Interior/G F Interior_page-0009.jpg',
+  '/interior/G-F-Interior/G F Interior_page-0010.jpg',
+  '/interior/SUDIKSHA-INTERIOR/SUDIKSHA INTERIOR_page-0002.jpg',
+  '/interior/SUDIKSHA-INTERIOR/SUDIKSHA INTERIOR_page-0004.jpg',
+  '/interior/SUDIKSHA-INTERIOR/SUDIKSHA INTERIOR_page-0005.jpg',
+  '/interior/SUDIKSHA-INTERIOR/SUDIKSHA INTERIOR_page-0006.jpg',
+  '/interior/SUDIKSHA-INTERIOR/SUDIKSHA INTERIOR_page-0007.jpg',
+  '/interior/SUDIKSHA-INTERIOR/SUDIKSHA INTERIOR_page-0008.jpg',
+  '/interior/KTICHEN-1_merged/KTICHEN 1_merged_page-0001.jpg',
+  '/interior/KTICHEN-1_merged/KTICHEN 1_merged_page-0003.jpg',
+  '/interior/KTICHEN-1_merged/KTICHEN 1_merged_page-0006.jpg',
+  '/interior/KTICHEN-1_merged/KTICHEN 1_merged_page-0008.jpg',
+  '/interior/KTICHEN-1_merged/KTICHEN 1_merged_page-0010.jpg',
+  '/interior/KTICHEN-1_merged/KTICHEN 1_merged_page-0012.jpg'
+];
+
 export default function Hero() {
+  const [images, setImages] = useState<string[]>([
+    '/exterior/Enscape_2023-12-02-12-32-02.png',
+    '/interior/SUDIKSHA-INTERIOR/SUDIKSHA INTERIOR_page-0004.jpg',
+    '/exterior/Enscape_2023-12-02-12-37-39.png',
+    '/interior/G-F-Interior/G F Interior_page-0008.jpg',
+    '/interior/KTICHEN-1_merged/KTICHEN 1_merged_page-0006.jpg'
+  ]);
+
+  useEffect(() => {
+    // Shuffle and pick distinct random images
+    const randomExteriors = [...exteriorImages].sort(() => 0.5 - Math.random()).slice(0, 2);
+    const randomInteriors = [...interiorImages].sort(() => 0.5 - Math.random()).slice(0, 3);
+    
+    if (randomExteriors.length >= 2 && randomInteriors.length >= 3) {
+      setImages([
+        randomExteriors[0],
+        randomInteriors[0],
+        randomExteriors[1],
+        randomInteriors[1],
+        randomInteriors[2]
+      ]);
+    }
+  }, []);
+
   return (
     <section className="hero">
       <div className="hero-bg-stripes"></div>
@@ -29,19 +94,19 @@ export default function Hero() {
 
         <div className="hero-image-gallery slide-up delay-200">
           <div className="gallery-card card-1">
-            <img src="/residential_villa_1775046517512.png" alt="Residential Villa Construction" />
+            <img src={images[0]} alt="Residential Villa Construction" />
           </div>
           <div className="gallery-card card-2">
-            <img src="/luxury_interior_design_1775046387018.png" alt="Luxury Interior" />
+            <img src={images[1]} alt="Luxury Interior" />
           </div>
           <div className="gallery-card card-3 center-card">
-            <img src="/commercial_building_1775046491084.png" alt="Commercial Building" />
+            <img src={images[2]} alt="Commercial Building" />
           </div>
           <div className="gallery-card card-4">
-            <img src="/modern_construction_site_1775046369628.png" alt="Modern Construction Site" />
+            <img src={images[3]} alt="Modern Construction Site" />
           </div>
           <div className="gallery-card card-5">
-            <img src="/architectural_blueprint_1775046419292.png" alt="Architectural Blueprint" />
+            <img src={images[4]} alt="Architectural Design" />
           </div>
         </div>
       </div>
